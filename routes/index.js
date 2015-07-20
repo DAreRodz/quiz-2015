689ebf6b696aa15fc590595b6637817640ff2b96
DAreRodz <darensr@gmail.com>
2015-07-20 22:49:23 +0200

    Página de autores

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function (req, res, next) {
+	res.render('author', { name: 'David Arenas Rodríguez', image: 'darenas.png' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
